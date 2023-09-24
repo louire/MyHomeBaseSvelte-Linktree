863c32b2f1b2a5ccafb1ed4a55249164e8e7befb
Loui Recio <lreciopersonal@gmail.com>
2023-09-24 16:58:49 -0500

    nothing, just the buttons on app.svelte and added npm installs

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,43 @@
-<script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
+<!--All needs to be responsive-->
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+.profile-picture {
+	width: 200px;
+	margin-top: 35px;
+	border-radius: 50%;
+	object-fit: cover;
+}
+.containerCentre {
+	text-align: center;
+}
+a.button {
+	background-color: #4CAF50;
+	border: none;
+	color: white;
+	padding: 15px 32px;
+	line-height: 24px;
+	display: inline-block;
+	font-size: 16px;
+	width: 80%;
+	margin-top: 20px;
+	border-radius: 10px;
+}
 </style>
+
+<div class="containerCentre">
+	<img src="https://avatars.githubusercontent.com/u/71304021?v=4" 
+	class="profile-picture"
+	alt="ProfilePicture, yeah its me :D"/>
+	<p>
+		@louire
+		<br>
+		Loui Recio Izaguirre
+	</p>
+	<a href="https://medium.com/@louirecio"target="_blank" class="button">medium</a>
+	<a href="https://twitter.com/louireciok" target="_blank" class="button">Twitter / X</a>
+	<a href="https://www.linkedin.com/in/louirecio/" target="_blank" class="button">LinkedIn</a>
+	<a href="https://github.com/louire" target="_blank" class="button" >GitHub</a>
+	
+</div>
+
+
